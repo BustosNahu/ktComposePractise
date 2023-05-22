6246f1223610c091a894c622ab707e3c9f2572e2
BustosNahu <nbustosalexander@gmail>
2023-05-22 10:58:48 -0300

    Simple button with Toast

--- app/src/main/java/com/example/ktcomposepractise_1/MainActivity.kt
+++ app/src/main/java/com/example/ktcomposepractise_1/MainActivity.kt
@@ -1,27 +1,40 @@
 package com.example.ktcomposepractise_1
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -35,51 +48,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SimpleText(Message(name, cuerpo))
-
-
+//            SimpleText(Message(name, cuerpo))
+            MyButton()
         }
     }
 }
 
 
 data class Message(val nombre: String, val cuerpo: String)
-
 @Composable
-fun SimpleText(msg: Message) {
-    Row() {
-        Spacer(modifier = Modifier
-            .width(10.dp)
-            .height(10.dp))
-        Image(
-            painter = painterResource(R.drawable.test_pic),
-            contentDescription = null,
-            modifier = Modifier
-                .size(45.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
-        )
+fun MyButton() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        val context = LocalContext.current
 
-        Spacer(modifier = Modifier.width(6.dp))
+        Button(onClick = {
+            Toast.makeText(context, "Hola Nahuel", Toast.LENGTH_SHORT).show()
+        },
+            modifier = Modifier.padding(all = Dp(10f)),
+            enabled = true,
+            shape = MaterialTheme.shapes.medium)
+        {
+            Text(text = "Geek for Geeks", color = Color.White)
 
-        Column {
-            Spacer(modifier = Modifier.height(10.dp))
-            Text(
-                text = msg.nombre,
-                Modifier.size(13.dp),
-                )
-            Spacer(modifier = Modifier.height(6.dp))
-            Text(text = msg.cuerpo)
         }
+
     }
 
 }
 
-@Preview(showBackground = true,
-            showSystemUi = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
-fun SimpleTextPreview() {
-    SimpleText(Message(name, cuerpo))
-
+fun DefaultPreview() {
+    MyButton()
 }
+
 
